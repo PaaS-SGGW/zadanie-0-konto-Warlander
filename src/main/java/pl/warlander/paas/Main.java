1c60ba1b6c663d12420f043cfbe534b665075378
Warlander <mackordian@gmail.com>
2018-12-09 01:59:25 +0100

    Added debug page

--- src/main/java/pl/warlander/paas/Main.java
+++ src/main/java/pl/warlander/paas/Main.java
@@ -1,12 +1,28 @@
 package pl.warlander.paas;
 
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
     
     public static void main(String[] args) {
         Spark.port(getHerokuAssignedPort());
-        Spark.staticFiles.location("/resources");
+        Spark.staticFiles.location("/");
+        
+        Spark.get("*", Main::handleMainPage);
+    }
+    
+    private static String handleMainPage(Request request, Response response) {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("title", "Heroku PaaS");
+        attributes.put("message", "Current route: " + request.pathInfo());
+        
+        return new FreeMarkerEngine().render(new ModelAndView(attributes, "index.ftl"));
     }
     
     private static int getHerokuAssignedPort() {
---
+++ src/main/resources/spark/template/freemarker/index.ftl
@@ -0,0 +1,14 @@
+<#ftl strip_whitespace = true>
+
+<#assign charset="UTF-8">
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>${title}</title>
+        <meta charset="${charset}">
+    </head>
+    <body>
+        ${message}
+    </body>
+</html>
